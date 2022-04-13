d10722ce055fc77b684f3bc4be135e4f82263420
tomeshsahu <tomeshwarsahu1980@gmail.com>
2022-04-14 00:22:45 +0530

    [TOMESHWAR]ADD:- PRIME NUMBER

---
+++ CoreLogicProgram/PrimeNumber.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreLogicProgram
+{
+    public class PrimeNumber
+    {
+        int count=0;
+        public void Prime()
+        {
+            Console.WriteLine("Check the Number is Prime or not");
+
+            int a=Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= a; i++)
+            {
+                if (a % i == 0)
+                {
+                    count++;
+                }
+            }
+                if(count==2)
+                {
+                    Console.WriteLine(a+" is Prime Number\n");
+                    }
+                else
+                {
+                    Console.WriteLine(a + " is Not Prime Number\n");
+                }
+            }
+        }
+    }
+
--- CoreLogicProgram/Program.cs
+++ CoreLogicProgram/Program.cs
@@ -2,19 +2,28 @@
 using CoreLogicProgram;
 
 Console.WriteLine("Welcome to Basic Program");
-Console.WriteLine("1.Factorial\n2.Perfect Number\n3.primeNumber\n4.Reverse Number");
-Console.WriteLine("Please Enter the Number Which program you want to run");
-int option = Convert.ToInt32(Console.ReadLine());
-
-switch (option)
+while (true)
 {
-    case 1:
-        RFactor Obj = new RFactor();
-        Obj.Factorial();
-        break;
+    Console.WriteLine("1.Factorial\n2.Perfect Number\n3.primeNumber\n4.Reverse Number");
+    Console.WriteLine("Please Enter the Number Which program you want to run");
+    int option = Convert.ToInt32(Console.ReadLine());
+
+    switch (option)
+    {
+        case 1:
+            RFactor Obj = new RFactor();
+            Obj.Factorial();
+            break;
+
+        case 2:
+            PrimeNumber prime = new PrimeNumber();
+            prime.Prime();
+            break;
 
 
-    default: Console.WriteLine("Please Enter Valid Number");
-        break;
+        default:
+            Console.WriteLine("Please Enter Valid Number");
+            break;
 
+    }
 }
