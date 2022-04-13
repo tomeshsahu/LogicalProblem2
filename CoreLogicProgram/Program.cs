ef9e89a4695516b5b2381545c2c90b8de3dbd7ac
tomeshsahu <tomeshwarsahu1980@gmail.com>
2022-04-14 00:04:50 +0530

    [Tomeshwar]Refactor:- Factorial

--- CoreLogicProgram/Program.cs
+++ CoreLogicProgram/Program.cs
@@ -1,6 +1,20 @@
 ﻿using System;
 using CoreLogicProgram;
 
+Console.WriteLine("Welcome to Basic Program");
+Console.WriteLine("1.Factorial\n2.Perfect Number\n3.primeNumber\n4.Reverse Number");
+Console.WriteLine("Please Enter the Number Which program you want to run");
+int option = Convert.ToInt32(Console.ReadLine());
 
-    RFactor Obj = new RFactor();
-    Obj.Factorial();
+switch (option)
+{
+    case 1:
+        RFactor Obj = new RFactor();
+        Obj.Factorial();
+        break;
+
+
+    default: Console.WriteLine("Please Enter Valid Number");
+        break;
+
+}
