fe916117fefb7923b8c419523ea3f23a103e9f98
tomeshsahu <tomeshwarsahu1980@gmail.com>
2022-04-14 23:32:11 +0530

    [TOMESHWAR]ADD: TEMPRATURE

--- CoreLogicProgram/Program.cs
+++ CoreLogicProgram/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using CoreLogicProgram;
 
+<<<<<<< Updated upstream
 class Program
 {
 
@@ -33,13 +34,57 @@ class Program
                     ReverseNumber Reverse = new ReverseNumber();
                     Reverse.Reverse();
                     break;
+=======
+Console.WriteLine("Welcome to Basic Program");
+bool end = true;
+while (end==true)
+{
+    Console.WriteLine("1.Factorial\n2.Perfect Number\n3.primeNumber\n4.Reverse Number\n5.Temprature");
+    Console.WriteLine("Please Enter the Number Which program you want to run");
+    int option = Convert.ToInt32(Console.ReadLine());
 
+    switch (option)
+    {
+        case 1:
+            RFactor Obj = new RFactor();
+            Obj.Factorial();
+            break;
+        case 2:
+            PerfectNumber perfect = new PerfectNumber();
+            perfect.Perfect();
+            break;
 
+        case 3:
+            PrimeNumber prime = new PrimeNumber();
+            prime.Prime();
+            break;
+
+        case 4:
+            ReverseNumber Reverse = new ReverseNumber();
+            Reverse.Reverse();
+            break;
+        case 5:
+            Temprature Conv = new Temprature();
+            Conv.Conversion(6,55);
+            break;
+>>>>>>> Stashed changes
+
+        case 0:
+            end = false;
+            break;
+
+<<<<<<< Updated upstream
                 default:
                     Console.WriteLine("Please Enter Valid Option" + "\n");
                     break;
 
             }
         }
+=======
+        default:
+            Console.WriteLine("Please Enter Valid Number");
+            break;
+
+>>>>>>> Stashed changes
     }
 }
---
+++ CoreLogicProgram/Temprature.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreLogicProgram
+{
+
+    public class Temprature
+    {
+        double F = 0;  double C;
+        public void Conversion(int a,int b) {
+            F=(a * 9 / 5) + 32 ;
+            Console.WriteLine("Fahrenheit" + " " + F);
+            C =(b - 32) * 5 / 9;
+            Console.WriteLine("Celcius"+" "+C);
+        }
+    }
+}
+
