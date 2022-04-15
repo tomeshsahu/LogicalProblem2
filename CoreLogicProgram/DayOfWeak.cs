afc2f6e7721679779365d4890d16b9d9dbef635d
tomeshsahu <tomeshwarsahu1980@gmail.com>
2022-04-15 08:18:48 +0530

    [TOMESHWAR]ADD:-DAY OF WEAK

---
+++ CoreLogicProgram/DayOfWeak.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreLogicProgram
+{
+    public class DayOfWeak
+    {
+        int day, year, month;
+        public void DOW(int day, int month, int year)
+        { 
+            this.day = day;
+            this.month = month;
+            this.year = year;
+            int year0 = year - (14 - month) / 12;
+            int leapyear = year0 + year0 / 4 - year0 / 100 + year0 / 400;
+            int month0 = month + 12 * ((14 - month) / 12) - 2;
+            int day0 = (day + (leapyear * month0) / 12) % 7;
+            switch (day0)
+            {
+
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("TuseDay");
+                    break;
+                case 3:
+                    Console.WriteLine("WednesDay");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunaday");
+                    break;
+
+            }
+        }
+        public void MOY(int day, int month, int year)
+        {
+            this.day = day;
+            this.month = month;
+            this.year = year;
+            int year0 = year - (14 - month) / 12;
+            int leapyear = year0 + year0 / 4 - year0 / 100 + year0 / 400;
+            int month0 = month + 12 * ((14 - month) / 12) - 2;
+            switch (month0)
+            {
+                case 1:
+                    Console.WriteLine("January");
+                    break;
+                case 2:
+                    Console.WriteLine("February");
+                    break;
+                case 3:
+                    Console.WriteLine("March");
+                    break;
+                case 4:
+                    Console.WriteLine("April");
+                    break;
+                case 5:
+                    Console.WriteLine("May");
+                    break;
+                case 6:
+                    Console.WriteLine("June");
+                    break;
+                case 7:
+                    Console.WriteLine("July");
+                    break;
+                case 8:
+                    Console.WriteLine("August");
+                    break;
+                case 9:
+                    Console.WriteLine("September");
+                    break;
+                case 10:
+                    Console.WriteLine("Auctober");
+                    break;
+                case 11:
+                    Console.WriteLine("November");
+                    break;
+                case 12:
+                    Console.WriteLine("December");
+                    break;
+                default:
+                    Console.WriteLine("Invailid Case");
+                    break;
+            }
+        }
+    }
+}
--- CoreLogicProgram/Program.cs
+++ CoreLogicProgram/Program.cs
@@ -3,9 +3,10 @@ using CoreLogicProgram;
 
 Console.WriteLine("Welcome to Basic Program");
 bool check = true;
+while(check)
 { 
 
-    Console.WriteLine("1.Factorial\n2.Perfect Number\n3.primeNumber\n4.Reverse Number\n5.Fibonacci\n6.Temprature Conversion");
+    Console.WriteLine("1.Factorial\n2.Perfect Number\n3.primeNumber\n4.Reverse Number\n5.Fibonacci\n6.Temprature Conversion\n7.DayOfWeak");
     Console.WriteLine("Please Enter the Number Which program you want to run");
     int option = Convert.ToInt32(Console.ReadLine());
 
@@ -37,6 +38,11 @@ bool check = true;
         case 6:
             Temprature conv = new Temprature();
             conv.Conversion(15,65);
+            break;
+        case 7:
+            DayOfWeak day = new DayOfWeak();
+            day.DOW(1, 1, 2016);
+            day.MOY(8, 8, 2016);
             break;
         case 0:
             check = false;
